0f6e4920cae27f04754e5cca55e7f08c969ebb0a
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2023-10-27 20:13:52 +0200

    Funciona boto desplegable

--- dropdown/drop.js
+++ dropdown/drop.js
@@ -1,17 +1,18 @@
-// JavaScript para mostrar u ocultar el menú desplegable al hacer clic en el botón "CV"
-document.getElementById("desplegableBoto").addEventListener("click", function() {
-    var dropdown = document.getElementById("myDropdown");
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
+// Obtén el botón y el menú desplegable
+var boton = document.getElementById("desplegableBoto");
+var dropdown = document.getElementById("myDropdown");
+
+// Añade el evento de escucha para el paso del ratón
+boton.addEventListener("click", function(event) {
+    console.log(event);
+    if (dropdown.hasAttribute("hidden")) {
+        dropdown.removeAttribute("hidden");
     } else {
-        dropdown.style.display = "block";
+        dropdown.setAttribute("hidden", "");
     }
 });
 
-// Cerrar el menú desplegable si se hace clic fuera de él
-window.addEventListener("click", function(event) {
-    if (event.target != document.getElementById("desplegableBoto") && event.target != document.getElementById("myDropdown")) {
-        var dropdown = document.getElementById("myDropdown");
-        dropdown.style.display = "none";
-    }
+// Añade el evento de escucha para cuando el ratón deja de pasar por encima
+dropdown.addEventListener("mouseleave", function() {
+    
 });
